83c8c8c88316abee03941056eb3196456626d619
Joey Kirk <joey.kirk@madebymunsters.com>
2015-11-24 15:13:11 -0500

    Added contact page

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var  mn = $(".secondary-nav");
+    mns = "secondary-nav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
